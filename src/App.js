b4097a5016a81b35f321fac97d52d6cfb9e9181c
OlliHeikkonen <olli.heikkonen@edu.salpaus.fi>
2021-03-15 21:50:21 +0000

    1532021

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Chart from "react-google-charts";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code>!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div style={{ display: 'flex', maxWidth: 1900 }}>
+  <Chart
+    width={1400}
+    height={300}
+    chartType="ColumnChart"
+    loader={<div>Loading Chart</div>}
+    data={[
+      ['City', '%'],
+      ['15.00', 27],
+      ['16.00', 27],
+      ['17.00', 26],
+      ['18.00', 26],
+      ['19.00', 26],
+    ]}
+    options={{
+      title: 'Ilmankosteus',
+      chartArea: { width: '30%' },
+      hAxis: {
+        title: 'Total Population',
+        minValue: 0,
+      },
+      vAxis: {
+        title: '',
+      },
+    }}
+    legendToggle
+  />
+  </div>
+   <div style={{ display: 'flex', maxWidth: 1900 }}>
+  <Chart
+    width={1400}
+    height={'300px'}
+    chartType="AreaChart"
+    loader={<div>Loading Chart</div>}
+    data={[
+      ['Year', 'C', ''],
+      ['15.00', 1000, 400],
+      ['16.00', 1170, 460],
+      ['17.00', 660, 1120],
+      ['18.00', 1030, 540],
+      ['19.00', 1030, 540],
+    ]}
+    options={{
+      title: 'Lämpötila',
+      hAxis: { title: 'Year', titleTextStyle: { color: '#333' } },
+      vAxis: { minValue: 0 },
+      // For the legend to fit, we make the chart area smaller
+      chartArea: { width: '50%', height: '70%' },
+      // lineWidth: 25
+    }}
+  />
+</div>
     </div>
   );
 }
